056fe8876e103417da19c8c6063e6086e124f55b
Zulkarnain Mohd <zulkarnain.md.ismail@gmail.com>
2020-10-08 11:47:15 +0200

    Pending improve front

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,23 +8,23 @@
 
 Article.destroy_all
 
-# def fake_content
-#   paragraphs = []
-#   paragraphs << Faker::Lorem.paragraph
-#   paragraphs << '![](http://lorempixel.com/800/300/city/)'
-#   paragraphs << "## #{Faker::Company.catch_phrase}"
-#   paragraphs << Faker::Lorem.paragraphs(2)
-#   paragraphs << "**#{Faker::Lorem.word}** #{Faker::Lorem.sentences.join}"
-#   paragraphs << "## #{Faker::Company.catch_phrase}"
-#   paragraphs << "#{Faker::Lorem.sentences.join} [#{Faker::Commerce.product_name}](#{Faker::Internet.url}) #{Faker::Lorem.sentences.join}"
-#   paragraphs << Faker::Lorem.paragraphs(3)
-#   paragraphs.flatten.join("\n\n")
-# end
+def fake_content
+  paragraphs = []
+  paragraphs << Faker::Lorem.paragraph
+  paragraphs << '![](http://lorempixel.com/800/300/city/)'
+  paragraphs << "## #{Faker::Company.catch_phrase}"
+  paragraphs << Faker::Lorem.paragraphs(number: 2)
+  paragraphs << "**#{Faker::Lorem.word}** #{Faker::Lorem.sentences.join}"
+  paragraphs << "## #{Faker::Company.catch_phrase}"
+  paragraphs << "#{Faker::Lorem.sentences.join} [#{Faker::Commerce.product_name}](#{Faker::Internet.url}) #{Faker::Lorem.sentences.join}"
+  paragraphs << Faker::Lorem.paragraphs(number: 3)
+  paragraphs.flatten.join("\n\n")
+end
 
 10.times do
   Article.create(
     title: Faker::Company.catch_phrase,
-    content: Faker::Lorem.paragraph
-    # content: fake_content
+    # content: Faker::Lorem.paragraph
+    content: fake_content
   )
 end
